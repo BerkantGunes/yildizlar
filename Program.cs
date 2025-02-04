44847585e2784dda98d81c1735b023dcb9aa0586
Berkant Gunes <gunesberkant96@gmail.com>
2025-02-04 17:00:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yildizlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Altalta yildiz olusturan program
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 5; i >= -1; i--) {
+                for (int j = 0; j < i; j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
